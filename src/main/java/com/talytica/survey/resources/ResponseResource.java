24e09910e0e414b6c47e45bbfb1db8627d423465
Sri <sridharkaza@yahoo.com>
2016-11-11 09:20:05 -0800

    cleaned up uncessary files and wrapped initial development on survey delivery app

--- src/main/java/com/talytica/survey/resources/ResponseResource.java
+++ src/main/java/com/talytica/survey/resources/ResponseResource.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import java.util.UUID;
 
 import javax.annotation.security.PermitAll;
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -17,14 +16,11 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.Response;
 
-import com.employmeo.data.model.Respondant;
 import com.employmeo.data.service.RespondantService;
-
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
