4ecbecd6c1d8f88acaa19f011f4dda16f5f08c20
Sri <sridharkaza@yahoo.com>
2017-08-10 16:38:22 -0700

    .

--- src/main/java/com/talytica/survey/SurveyApplication.java
+++ src/main/java/com/talytica/survey/SurveyApplication.java
@@ -3,6 +3,7 @@ package com.talytica.survey;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
@@ -13,6 +14,7 @@ import com.amazonaws.services.s3.AmazonS3Client;;
 		exclude={MultipartAutoConfiguration.class},
 		scanBasePackages = {"com.employmeo.data", "com.talytica.survey", "com.talytica.common", "com.talytica.metrics"}
 		)
+@EnableCaching
 public class SurveyApplication {
 
 	public static void main(String[] args) {
